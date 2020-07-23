0cc29010903d5f5fe167d345ac40fc90e2ba20fb
Greg Kluska <kluskagreg@gmail.com>
2020-07-23 22:52:39 +0100

    Add data to UI

--- app/src/main/java/com/gregkluska/jetpackcomposeplayground/ui/main/MainActivity.kt
+++ app/src/main/java/com/gregkluska/jetpackcomposeplayground/ui/main/MainActivity.kt
@@ -6,14 +6,13 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.lifecycle.Observer
-import androidx.ui.core.ContentScale
-import androidx.ui.core.Modifier
-import androidx.ui.core.clip
-import androidx.ui.core.setContent
+import androidx.ui.core.*
 import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.VerticalScroller
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.layout.*
+import androidx.ui.material.Card
 import androidx.ui.material.MaterialTheme
 import androidx.ui.res.imageResource
 import androidx.ui.text.style.TextOverflow
@@ -23,6 +22,7 @@ import com.gregkluska.jetpackcomposeplayground.R
 import com.gregkluska.jetpackcomposeplayground.api.ApiEmptyResponse
 import com.gregkluska.jetpackcomposeplayground.api.ApiErrorResponse
 import com.gregkluska.jetpackcomposeplayground.api.ApiSuccessResponse
+import com.gregkluska.jetpackcomposeplayground.models.Post
 import dagger.hilt.android.AndroidEntryPoint
 
 private const val TAG = "MainActivity"
@@ -35,10 +35,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
-            NewsStory()
-        }
-
         subscribeObservers()
     }
 
@@ -48,6 +44,10 @@ class MainActivity : AppCompatActivity() {
             when (response) {
                 is ApiSuccessResponse -> {
                     Log.d(TAG, "subscribeObservers: Success response : ${response.body}")
+
+                    setContent {
+                        PopulateList(response.body)
+                    }
                 }
                 
                 is ApiErrorResponse -> {
@@ -65,29 +65,31 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun NewsStory() {
+fun NewsStory(title: String, body: String) {
     val image = imageResource(R.drawable.header)
     MaterialTheme {
         val typography = MaterialTheme.typography
-        Column (
-            modifier = Modifier.padding(16.dp)
-        )
-        {
+        Column() {
             val imageModifier = Modifier
                 .preferredHeightIn(maxHeight = 180.dp)
                 .fillMaxWidth()
-                .clip(shape = RoundedCornerShape(4.dp))
 
-            Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
-            Spacer(Modifier.preferredHeight(16.dp))
-            Text("A day in Shark Fin Cove",
-                style = typography.h6,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis)
-            Text("Davenport, California",
-                style = typography.body2)
-            Text("December 2018",
-                style = typography.body2)
+
+            Column (
+                modifier = Modifier.padding(2.dp)
+            ) {
+                Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
+                Column (
+                        modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(title,
+                            style = typography.h6,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis)
+                    Text(body, style = typography.body2)
+                    Text("December 2018", style = typography.body2)
+                }
+            }
         }
     }
 }
@@ -95,5 +97,16 @@ fun NewsStory() {
 @Preview
 @Composable
 fun DefaultPreview() {
-    NewsStory()
+    NewsStory("This is the title", "Lorem Ipsum Body")
+}
+
+@Composable
+fun PopulateList(posts: List<Post>) {
+    VerticalScroller {
+        Column {
+            posts.forEach {
+                NewsStory(it.title, it.body)
+            }
+        }
+    }
 }
